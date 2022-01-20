81b0fdf99e6feb234883e9a45e3b0406daf80662
Ritesh Patel <patel2000ritesh@gmail.com>
2022-01-19 21:10:28 -0500

    Inital commit

---
+++ src/components/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>Shopify Image Repo</h1>
+      <h2>Your Images</h2>
+      <p>By Ritesh Patel: Welcome to your gallery</p>
+    </div>
+  )
+}
+
+export default Title;
